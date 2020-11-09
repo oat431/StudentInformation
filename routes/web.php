86d99a28560e13e4f545c86a9dd6e598363ec14d
Oralita <oat431@gmail.com>
2020-11-09 20:54:32 +0700

    add funcitonal routes to the main system

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
             return redirect("/student/".Auth::user()->id);
         }
         if(Auth::user()->role == 'admin'){
-            return redirect("/home");
+            return redirect("/admin");
         }
     }
     return view("Master.master");
@@ -33,11 +33,13 @@ Route::resource('registration', 'App\Http\Controllers\RegistrationController');
 /**end */
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(CheckStatus::class);
 
+Route::get('/admin','App\Http\Controllers\AdminCon@index');
 Route::get('/admin/delete/{id}', 'App\Http\Controllers\AdminCon@destroy')->name('admin.destroy');
 Route::get('/grade', function () {
     $data = DB::table('users')->whereRaw('approve = 1 and id <> 1')->get();
     return view('admin.grade', compact(['data']));
 });
+
 Route::get('/updategrade', 'App\Http\Controllers\AdminCon@update');
 
 Route::get('/student/{id}', 'App\Http\Controllers\StudentController@show');
@@ -45,7 +47,14 @@ Route::get('/enroll/{id}', 'App\Http\Controllers\CourseController@show');
 
 Route::get('/registration/delete/{id}',"App\Http\Controllers\RegistrationController@destroy")->name('registration.destroy');
 Route::post('/registration', "App\Http\Controllers\RegistrationController@store");
+
+Route::get('/student/delete/{id}','App\Http\Controllers\StudentController@destroy')->name('student.destroy');
 Route::post('/student', 'App\Http\Controllers\StudentController@store');
 Route::post('/student/update/{id}','App\Http\Controllers\StudentController@update');
+
+Route::get('/course/delete/{id}','App\Http\Controllers\CourseController@destroy');
+Route::post('/course/update/{id}','App\Http\Controllers\CourseController@update');
+Route::post('/course','App\Http\Controllers\CourseController@store');
+
 Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login');
 Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout');
