dcfd4f2615839bdd6b8d86b33ee7da3ef310f7d8
blackneko1234 <blackneko1234@hotmail.com>
2020-11-10 18:50:41 +0700

    modofy some filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -66,7 +66,8 @@ return [
         ],
         'public_uploads' => [
             'driver' => 'local',
-            'root'   => public_path(),
+            'root'   => public_path('/pic/'),
+            'visibility' => 'public',
         ],
     ],
 
